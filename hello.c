6472156c1a8f7c5c6989338d3b2f589a47174235
richardkolodziej1234 <42583117+richardkolodziej1234@users.noreply.github.com>
2018-08-23 11:25:13 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Richard Kolodziej and Daniel Pankey
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Richard Kolodziej... Computer Science\n");
+
+  return 0;
+}
